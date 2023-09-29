5bc3f683a248667e9f1241fe2fc34a7299dff69e
Denitsa Slavkova <deni9309@gmail.com>
2023-09-29 17:20:47 +0300

    Added layout for about-us and terms pages

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-about-us',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AboutUsComponent {
 
+    constructor(private router: Router) { }
+
+    navigateToTermsPage(): void{
+        this.router.navigate([ '/terms-and-conditions' ]);
+    }
+
+    navigateToHomePage(): void {
+        this.router.navigate([ '/home' ]);
+    }
 }
--- src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-terms-and-conditions',
-  templateUrl: './terms-and-conditions.component.html',
-  styleUrls: ['./terms-and-conditions.component.css']
+    selector: 'app-terms-and-conditions',
+    templateUrl: './terms-and-conditions.component.html',
+    styleUrls: [ './terms-and-conditions.component.css' ]
 })
 export class TermsAndConditionsComponent {
+    
+    constructor(private router: Router) { }
 
+    navigateToHomePage(): void {
+        this.router.navigate([ '/home' ]);
+    }
 }
