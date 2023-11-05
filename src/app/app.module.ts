1411c5e5d5969717679b9015c954e112fcbdd4d1
Denitsa Slavkova <deni9309@gmail.com>
2023-11-05 18:42:00 +0200

    Connect to firebase; added additional packages via npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
+import { ToastrModule } from 'ngx-toastr';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { environment } from 'src/environments/environment.variables';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
-import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
 import { SinglePostComponent } from './pages/single-post/single-post.component';
-import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
-
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
-import { ContactUsComponent } from './pages/contact-us/contact-us.component';
-import { PostCardComponent } from './layouts/post-card/post-card.component';
+
 
 @NgModule({
     declarations: [
@@ -37,9 +49,21 @@ import { PostCardComponent } from './layouts/post-card/post-card.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        BrowserAnimationsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideFirestore(() => getFirestore()),
+        provideAuth(() => getAuth()),
+        provideStorage(() => getStorage()),
+        ToastrModule.forRoot(),
+        TooltipModule.forRoot(),
+        ModalModule.forRoot(),
     ],
-    providers: [],
+    providers: [
+        ScreenTrackingService,
+        UserTrackingService
+    ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
